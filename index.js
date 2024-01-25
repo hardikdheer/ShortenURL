a04a3ed35040a0df898eeef262d02eacef9147c6
hardikdheer <hardikdheer12@gmail.com>
2024-01-25 16:21:24 +0530

    Server Side Rendering

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express=require("express")
-const {connectMongoDb}=require("./connect")
-const URL=require("./models/url")
-const urlRoute=require("./routes/url")
-const app=express()
-const PORT=8001
+const express = require("express")
+const path = require("path")
+const { connectMongoDb } = require("./connect")
+const URL = require("./models/url")
+const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticrouter")
+const app = express()
+const PORT = 8001
 
-connectMongoDb("mongodb://127.0.0.1:27017/short-url").then(()=>console.log("MongoDB connected"))
+connectMongoDb("mongodb://127.0.0.1:27017/short-url").then(() => console.log("MongoDB connected"))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
-app.use("/url",urlRoute);
-app.get("/:shortId",async(req,res)=>{
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls
+    })
+});
+
+app.use("/url", urlRoute);
+app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId
     const entry = await URL.findOneAndUpdate({
         shortId
@@ -21,4 +34,5 @@ app.get("/:shortId",async(req,res)=>{
     );
     return res.redirect(entry.redirectURL);
 })
-app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
+app.use("/", staticRoute);
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
